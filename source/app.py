60f698da0ce42fce0c963a05c3b24a851a89ee30
Rory Coe <el20rdc@leeds.ac.uk>
2022-12-20 15:01:25 +0000

    Scaling everything back to make it less overwhelming to work on - VERY basic sessions working

--- source/app.py
+++ source/app.py
@@ -1,26 +1,66 @@
-from flask import Flask
-from flask_restful import Api
-
-# Import the different sections of the API
-from contacts.api import Contacts, Contact
-from modules.api import Modules, Module
-from residents.api import Residents, Resident
-from users.api import Users, User
+import redis as redis
+from flask import Flask, session, request
+from flask_restful import Api, Resource
+from flask_session import Session
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Contacts, '/contacts/')
-api.add_resource(Contact, '/contacts/<contact_id>')
+# Details on the Secret Key: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
+# NOTE: The secret key is used to cryptographically-sign the cookies used for storing
+#       the session identifier.
+app.secret_key = 'temporary'
 
-api.add_resource(Modules, '/modules/')
-api.add_resource(Module, '/modules/<module_id>')
+# Configure Redis for storing the session data on the server-side
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis.from_url('redis://localhost:6379')
 
-api.add_resource(Residents, '/residents/')
-api.add_resource(Resident, '/residents/<resident_id>')
+# Create and initialize the Flask-Session object AFTER `app` has been configured
+server_session = Session(app)
+
+
+class User(Resource):
+    def get(self, username):
+        if session.get("username") == username:
+            return {"user": username, "permissions": session.get("permissions")}
+        return {"user": username}
+
+    def put(self, username):
+        return {'page': 'user put'}, 201
+
+    def delete(self, username):
+        return '', 204
+
+
+class Users(Resource):
+    def get(self):
+        return {'page': 'users get'}
+
+    def post(self):
+        return {'page': 'users post'}
+
+
+class UserSession(Resource):
+    def post(self):
+        args = request.get_json(force=True)
+
+        session['username'] = args["username"]
+        session['permissions'] = args["permissions"]
+
+        return {'page': 'users post'}
+
+    def delete(self):
+        session.pop('username', default=None)
+        session.pop('permissions', default=None)
+
+        return '', 204
+
 
 api.add_resource(Users, '/users/')
-api.add_resource(User, '/users/<user_id>')
+api.add_resource(User, '/users/<username>')
+api.add_resource(UserSession, '/login/')
 
 if __name__ == '__main__':
     app.run(debug=True)
