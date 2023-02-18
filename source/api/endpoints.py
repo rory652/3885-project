7b78c114f6199b5cb1f64a0f77ed043e026f79e7
Rory Coe <el20rdc@leeds.ac.uk>
2023-02-18 12:38:42 +0000

    Moved all the endpoint processing classes into another file and did some slight refactoring to remove some repeated code.

---
+++ source/api/endpoints.py
@@ -0,0 +1,319 @@
+from flask import Flask, session, request
+from flask_restful import Api, Resource
+
+import databases.users as users
+import databases.contacts as contacts
+import databases.residents as residents
+import databases.modules as modules
+import databases.locations as locations
+
+# Different database objects
+userDB = None
+contactDB = None
+locationDB = None
+residentDB = None
+moduleDB = None
+
+# Permissions:
+#   0 - Resident
+#   1 - Nurse
+#   2 - Admin
+#   -1 - Module
+PERMISSIONS = {"resident": 0, "nurse": 1, "admin": 2, "module": -1}
+
+
+def initialiseDB(password):
+    global userDB, contactDB, locationDB, residentDB, moduleDB
+
+    userDB = users.Users(password)
+    contactDB = contacts.Contacts(password)
+    locationDB = locations.Locations(password)
+    residentDB = residents.Residents(password)
+    moduleDB = modules.Modules(password)
+
+
+def validateUser(userSession, permission):
+    if "username" not in userSession:
+        return "user not logged in", 401
+
+    if session.get("permissions") < permission:
+        return "invalid credentials", 403
+
+    return False
+
+
+class Contact(Resource):
+    def delete(self, contact_id):
+        if not contactDB.contains(contact_id):
+            return "resource not found", 404
+
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        contactDB.delete(contact_id)
+        return '', 204
+
+
+class Contacts(Resource):
+    def get(self):
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        return contactDB.get()
+
+
+class Locations(Resource):
+    def post(self):
+        if "username" not in session:
+            return "user not logged in", 401
+
+        if session.get("permissions") != PERMISSIONS["module"]:
+            return "invalid credentials", 403
+
+        args = request.get_json(force=True)
+
+        if "wearable" not in args:
+            return "wearable not set", 400
+
+        if "coordinates" not in args:
+            return "coordinates not set", 400
+
+        return locationDB.add(session.get("username"), args["wearable"], str(args["coordinates"]))
+
+
+class Module(Resource):
+    def get(self, module_id):
+        if not moduleDB.contains(module_id):
+            return "resource not found", 404
+
+        if toReturn := validateUser(session, PERMISSIONS["admin"]):
+            return toReturn
+
+        return moduleDB.get(module_id)
+
+    def put(self, module_id):
+        if not moduleDB.contains(module_id):
+            return "resource not found", 404
+
+        if "username" not in session:
+            return "no user logged in", 401
+
+        if not session.get("permissions") == PERMISSIONS["module"]:
+            return "invalid user", 403
+
+        args = request.get_json(force=True)
+
+        if "new-status" not in args:
+            args["new-status"] = "no status"
+
+        newInfo = moduleDB.update(module_id, args["new-status"], args["new-room"])
+        if newInfo is None:
+            return "field not set", 400
+
+        return newInfo, 201
+
+    def delete(self, module_id):
+        if not moduleDB.contains(module_id):
+            return "resource not found", 404
+
+        if toReturn := validateUser(session, PERMISSIONS["admin"]):
+            return toReturn
+
+        moduleDB.delete(module_id)
+        return '', 204
+
+
+class Modules(Resource):
+    def get(self):
+        if toReturn := validateUser(session, PERMISSIONS["admin"]):
+            return toReturn
+
+        return moduleDB.get()
+
+    def post(self):
+        if toReturn := validateUser(session, PERMISSIONS["admin"]):
+            return toReturn
+
+        args = request.get_json(force=True)
+
+        if "room" not in args:
+            return "room not set", 400
+
+        if "status" not in args:
+            args["status"] = "no status"
+
+        return moduleDB.add(args["room"], args["status"])
+
+
+class Resident(Resource):
+    def get(self, resident_id):
+        if not residentDB.contains(resident_id):
+            return "resource not found", 404
+
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        return residentDB.get(resident_id)
+
+    def put(self, resident_id):
+        if not residentDB.contains(resident_id):
+            return "resource not found", 404
+
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        args = request.get_json(force=True)
+
+        if "new-name" not in args:
+            args["new-name"] = None
+
+        if "new-wearable" not in args:
+            args["new-wearable"] = None
+
+        if "new-status" not in args:
+            args["new-status"] = None
+
+        newInfo = residentDB.update(resident_id, args["new-name"], args["new-wearable"], args["new-status"])
+        if newInfo is None:
+            return "field not set", 400
+
+        return "resident updated", 201
+
+    def delete(self, resident_id):
+        if not residentDB.contains(resident_id):
+            return "resource not found", 404
+
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        residentDB.delete(resident_id)
+        return '', 204
+
+
+class Residents(Resource):
+    def get(self):
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        return residentDB.get()
+
+    def post(self):
+        if toReturn := validateUser(session, PERMISSIONS["nurse"]):
+            return toReturn
+
+        args = request.get_json(force=True)
+
+        if "name" not in args:
+            return "name not set", 400
+
+        if "wearable" not in args:
+            return "wearable not set", 400
+
+        if "status" not in args:
+            args["status"] = "false"
+
+        return residentDB.add(args["name"], args["wearable"], args["status"])
+
+
+class User(Resource):
+    def get(self, username):
+        if not userDB.contains(username):
+            return "resource not found", 404
+
+        if "username" not in session:
+            return "no user logged in", 401
+
+        if not session.get("username") == username:
+            return "invalid user", 403
+
+        return userDB.get(username)
+
+    def put(self, username):
+        if not userDB.contains(username):
+            return "resource not found", 404
+
+        if "username" not in session:
+            return "no user logged in", 401
+
+        if not session.get("username") == username:
+            return "invalid user", 403
+
+        args = request.get_json(force=True)
+
+        # Verify that the user is legitimate
+        if not userDB.verify(args["username"], args["password"]):
+            return "invalid credentials", 400
+
+        if "new-username" not in args:
+            args["new-username"] = None
+
+        if "new-password" not in args:
+            args["new-password"] = None
+
+        newUsername, newInfo = userDB.update(args["username"], args["new-username"], args["new-password"])
+        if newInfo is None:
+            return "field not set", 400
+
+        # Update session
+        session['username'] = newUsername
+        session['permissions'] = newInfo["permissions"]
+        return "user updated", 201
+
+    def delete(self, username):
+        if not userDB.contains(username):
+            return "resource not found", 404
+
+        if "username" not in session:
+            return "no user logged in", 401
+
+        if not session.get("username") == username:
+            return "invalid user", 403
+
+        userDB.delete(username)
+
+        # Delete the session
+        session.pop('username', default=None)
+        session.pop('permissions', default=None)
+        return '', 204
+
+
+class Users(Resource):
+    def get(self):
+        if toReturn := validateUser(session, PERMISSIONS["admin"]):
+            return toReturn
+
+        return userDB.get()
+
+    def post(self):
+        args = request.get_json(force=True)
+
+        permissions = 0
+        if "permissions" in args:
+            permissions = args["permissions"]
+
+        if not userDB.add(args["username"], args["password"], permissions):
+            return "failed to add user", 400
+
+        # Create a session
+        session['username'] = args["username"]
+        session['permissions'] = permissions
+        return "user added", 201
+
+
+class UserSession(Resource):
+    def post(self):
+        args = request.get_json(force=True)
+
+        if userDB.verify(args["username"], args["password"]):
+            session['username'] = args["username"]
+            session['permissions'] = userDB.get(args["username"])["permissions"]
+
+            return "session created", 201
+
+        return "invalid login credentials", 400
+
+    def delete(self):
+        session.pop('username', default=None)
+        session.pop('permissions', default=None)
+
+        return '', 204
