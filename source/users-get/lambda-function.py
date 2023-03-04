fe1a6fbe09db16c13a4ffc0cddf88ab1e45cfb5a
Rory Coe <el20rdc@leeds.ac.uk>
2023-03-04 20:34:09 +0000

    Basic Lambdas Added for each endpoint

---
+++ source/users-get/lambda-function.py
@@ -0,0 +1,11 @@
+import boto3
+
+client = boto3.client('dynamodb')
+
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'endpoint': 'users',
+        'request': 'get'
+    }
