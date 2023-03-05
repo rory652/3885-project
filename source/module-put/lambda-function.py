697d2d3f0487763f450525df70686ced03f00e1a
Rory Coe <el20rdc@leeds.ac.uk>
2023-03-05 21:13:17 +0000

    module-put lambda implemented

--- source/module-put/lambda-function.py
+++ source/module-put/lambda-function.py
@@ -1,11 +1,80 @@
-import boto3
+import json, boto3
+from boto3.dynamodb.conditions import Key
 
-client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('modules')
+
+
+def generateKey(carehome, id):
+    return {
+        'carehome': carehome,
+        'id': id,
+    }
+
+
+def generateAttributes(**attributes):
+    update = {}
+
+    for key, value in attributes.items():
+        if value != "":
+            update[key] = {"Value": value}
+
+    return update
 
 
 def lambda_handler(event, context):
+    carehome = event["pathParameters"]["carehomeId"]
+    if type(carehome) != str:
+        carehome = str(carehome)
+
+    module = event["pathParameters"]["moduleId"]
+
+    try:
+        body = event["body"]
+        if type(event["body"]) == str:
+            body = json.loads(body)
+
+        new_room = body["new_room"]
+        new_status = body["new_status"]
+    except KeyError as err:
+        return {
+            'statusCode': 400,
+            'headers': {},
+            'body': json.dumps({
+                'error': f'{str(err)} field missing - set to empty string to not update'
+            }),
+            "isBase64Encoded": False,
+        }
+
+    # Validate inputs here
+    if not validateId(carehome, module):
+        return {
+            'statusCode': 404,
+            'headers': {},
+            'body': json.dumps({
+                'error': 'module not found'
+            }),
+            "isBase64Encoded": False,
+        }
+
+    keys = generateKey(carehome, module)
+    attributes = generateAttributes(room=new_room, status=new_status)
+
+    response = table.update_item(Key=keys, AttributeUpdates=attributes)
+
     return {
-        'statusCode': 200,
-        'endpoint': 'module',
-        'request': 'PUT'
+        'statusCode': 201,
+        'headers': {},
+        'body': json.dumps({
+            'database-status': response["ResponseMetadata"]["HTTPStatusCode"]
+        }),
+        "isBase64Encoded": False,
     }
+
+
+def validateId(carehome, moduleId):
+    response = table.query(
+        KeyConditionExpression=Key('carehome').eq(carehome) & Key('id').eq(moduleId)
+    )
+
+    return len(response["Items"]) > 0
