44618471785dd2e5c4c466c3d705fdf35d653c2a
Rory Coe <el20rdc@leeds.ac.uk>
2023-03-05 13:52:55 +0000

    residents-get lambda implemented

--- source/residents-get/lambda-function.py
+++ source/residents-get/lambda-function.py
@@ -1,11 +1,24 @@
-import boto3
+import json, boto3
+from boto3.dynamodb.conditions import Key
 
-client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('residents')
 
 
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'endpoint': 'residents',
-        'request': 'GET'
+        'headers': {},
+        'body': json.dumps({
+            'residents': fetchResidents(event["pathParameters"]["carehomeId"])
+        }),
+        "isBase64Encoded": False,
     }
+
+
+def fetchResidents(carehome):
+    response = table.query(
+        KeyConditionExpression=Key('carehome').eq(carehome)
+    )
+
+    return response["Items"]
