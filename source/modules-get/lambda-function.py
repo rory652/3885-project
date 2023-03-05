24bbfa1812420939fbd27c7298ea267c92d507b0
Rory Coe <el20rdc@leeds.ac.uk>
2023-03-05 14:03:12 +0000

    other multi-get lambdas implemented

--- source/modules-get/lambda-function.py
+++ source/modules-get/lambda-function.py
@@ -1,11 +1,24 @@
-import boto3
+import json, boto3
+from boto3.dynamodb.conditions import Key
 
-client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('modules')
 
 
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'endpoint': 'modules',
-        'request': 'GET'
+        'headers': {},
+        'body': json.dumps({
+            'modules': fetcModules(event["pathParameters"]["carehomeId"])
+        }),
+        "isBase64Encoded": False,
     }
+
+
+def fetcModules(carehome):
+    response = table.query(
+        KeyConditionExpression=Key('carehome').eq(carehome)
+    )
+
+    return response["Items"]
--- source/users-get/lambda-function.py
+++ source/users-get/lambda-function.py
@@ -1,11 +1,24 @@
-import boto3
+import json, boto3
+from boto3.dynamodb.conditions import Key
 
-client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('users')
 
 
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'endpoint': 'users',
-        'request': 'get'
+        'headers': {},
+        'body': json.dumps({
+            'users': fetchUsers(event["pathParameters"]["carehomeId"])
+        }),
+        "isBase64Encoded": False,
     }
+
+
+def fetchUsers(carehome):
+    response = table.query(
+        KeyConditionExpression=Key('carehome').eq(carehome)
+    )
+
+    return response["Items"]
