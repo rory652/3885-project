771bbfdc09a5dd6abc5326c41c0751dde9728ac0
Rory Coe <el20rdc@leeds.ac.uk>
2023-03-05 20:08:55 +0000

    residents-post implemented without input validation

--- source/residents-post/lambda-function.py
+++ source/residents-post/lambda-function.py
@@ -1,11 +1,77 @@
-import boto3
+import json, boto3
+from boto3.dynamodb.conditions import Key
+from secrets import token_hex
 
-client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('residents')
+
+
+def generateItem(carehome, id, name, status, wearable):
+    return {
+        'carehome': carehome,
+        'id': id,
+        'name': name,
+        'status': status,
+        'wearableId': wearable,
+    }
 
 
 def lambda_handler(event, context):
+    carehome = event["pathParameters"]["carehomeId"]
+
+    if type(carehome) != str:
+        carehome = str(carehome)
+
+    try:
+        body = event["body"]
+        if type(event["body"]) == str:
+            body = json.loads(body)
+
+        name = body["name"]
+        status = body["status"]
+        wearable = body["wearableId"]
+    except KeyError as err:
+        return {
+            'statusCode': 400,
+            'headers': {},
+            'body': json.dumps({
+                'error': f'{str(err)} field missing'
+            }),
+            "isBase64Encoded": False,
+        }
+
+    # Validate inputs here
+    status = True if body["status"] == "true" else False
+
+    residentId = generateId(carehome)
+
+    response = table.put_item(Item=generateItem(carehome, residentId, name, status, wearable))
+
     return {
-        'statusCode': 200,
-        'endpoint': 'residents',
-        'request': 'POST'
+        'statusCode': 201,
+        'headers': {},
+        'body': json.dumps({
+            'database-status': response["ResponseMetadata"]["HTTPStatusCode"]
+        }),
+        "isBase64Encoded": False,
     }
+
+
+def generateId(carehome):
+    valid = False
+    generated = token_hex(8)
+
+    while not valid:
+        response = table.query(
+            KeyConditionExpression=Key('carehome').eq(carehome) & Key('id').eq(generated)
+        )
+
+        print(response)
+
+        if len(response["Items"]) == 0:
+            valid = True
+        else:
+            generated = token_hex(8)
+
+    print(generated)
+    return generated
