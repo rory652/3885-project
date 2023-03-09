7cab811cefb41e7a376ce046ce1f24b05fff832e
Rory Coe <el20rdc@leeds.ac.uk>
2023-03-09 22:43:57 +0000

    login-post and login-delete somehow got switched over

--- source/login-delete/lambda_function.py
+++ source/login-delete/lambda_function.py
@@ -31,78 +31,27 @@ def generateResponse(status, body, headers=None):
 def lambda_handler(event, context):
     carehome = event["pathParameters"]["carehomeId"]
 
-    if type(carehome) != str:
+    if not isinstance(carehome, str):
         carehome = str(carehome)
 
     try:
-        body = event["body"]
-        if type(event["body"]) == str:
-            body = json.loads(body)
+        headers = event["headers"]
+        if isinstance(headers, str):
+            headers = json.loads(headers)
 
-        username = body["username"]
-        password = body["password"]
+        session = headers["SESSION-ID"]
     except KeyError as err:
         return generateResponse(400, {'error': f'{str(err)} field missing'})
 
-    user = getUser(carehome, username)
-
-    # Validate inputs here
-    if len(user) == 0:
-        return generateResponse(400, {'error': 'user not found'})
-
-    if checkSession(carehome, username):
-        return generateResponse(400, {'error': 'user already logged in'})
-
-    if hashPassword(password, user[0]["salt"]) != user[0]["password"]:
-        return generateResponse(400, {'error': 'incorrect password'})
-
-    sessionId = generateSession(carehome)
-    response = table.put_item(Item=generateItem(carehome, sessionId, username, user[0]["role"]))
-
-    return generateResponse(200, {'database-status': response["ResponseMetadata"]["HTTPStatusCode"]}, headers={"SESSION-ID": sessionId})
+    return generateResponse(204, {'database-status': delete(carehome, session)["ResponseMetadata"]["HTTPStatusCode"]})
 
 
-def getUser(carehome, username):
-    response = users.query(
-        KeyConditionExpression=Key('carehome').eq(carehome) & Key('username').eq(username)
+def delete(carehome, session):
+    response = table.delete_item(
+        Key={
+            'carehome': carehome,
+            'id': session
+        }
     )
 
-    return response["Items"]
-
-
-def checkSession(carehome, username):
-    response = table.query(
-        KeyConditionExpression=Key('carehome').eq(carehome)
-    )
-
-    for session in response["Items"]:
-        if session["username"] == username:
-            return True
-
-    return False
-
-
-def generateSession(carehome):
-    valid = False
-    generated = token_hex(8)
-
-    while not valid:
-        response = table.query(
-            KeyConditionExpression=Key('carehome').eq(carehome) & Key('id').eq(generated)
-        )
-
-        if len(response["Items"]) == 0:
-            valid = True
-        else:
-            generated = token_hex(8)
-
-    return generated
-
-
-def hashPassword(password, salt):
-    h = sha256("".join([password, salt]).encode()).digest()
-    # Repeatedly hash - slow down algorithm
-    for i in range(100):
-        h = sha256(h).digest()
-
-    return sha256(h).hexdigest()
+    return response
